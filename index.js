03a84b7cf4b2d2060ced6feca21ce89f9de0515f
Lee Pin-Tzu <snylee1021.cs08@nycu.edu.tw>
2024-09-23 01:01:25 -0700

    fix pyleotutorial icon, add tab for separated works, add project3

--- index.js
+++ index.js
@@ -41,3 +41,22 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";  
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(tabName).style.display = "block";  
+  evt.currentTarget.classList.add("active");
+}
+
+// Default open the first tab
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".tablinks").click(); // Click the first tab
+});
